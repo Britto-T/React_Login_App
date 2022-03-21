a940d2e4910b4ac6cebc61dcf3f4145d7d9c2b9f
Britto-T <meetbrittohere@gmail.com>
2022-03-21 17:56:09 +0530

    Style added

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,19 +1,27 @@
 import React from "react"
 import "./Home.css"
+import UserDetails from "./UserDetails";
 
 const Login=()=>{
     return (
+      <>
       <div className="fieldWrapper">
-           <label>User Name</label>
-           <input type="text"/>
-           <label>Phone Number</label>
-           <input type="text"/>
-           <label>E-Mail</label>
-           <input type="text"/>
-           <label>Zip Code</label>
-           <input type="text"/>
-           <button className="btnSubmit">Submit</button>
+           <div><label>User Name</label></div>
+           <div><input type="text" placeholder="User Name"/></div>
+           <div><label>Phone Number</label></div>
+           <div><input type="text" placeholder="Phone Number"/></div>
+           <div><label>E-Mail</label></div>
+           <div><input type="text" placeholder="E-Mail"/></div>
+           <div><label>Zip Code</label></div>
+           <div><input type="text" placeholder="Zip Code"/></div>
+           <div>
+              <button className="btnSubmit">Submit</button>
+              <button className="btnReset">Reset</button>
+           </div>
       </div>
+      <UserDetails/>
+      </>
+     
     );
 }
 
--- src/UserDetails.tsx
+++ src/UserDetails.tsx
@@ -1,2 +1,50 @@
-import react from "react"
+import react from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Home.css";
 
+const UserDetails = () => {
+  const [studentDetails, setStudentDetails] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response: any) => {
+        setStudentDetails(response.data);
+        console.log(response.data.length);
+      })
+      .catch((error: any) => {});
+     
+    },[]);
+
+  const counterHandler=(event:any)=>{
+        console.log(event.target.value)
+        studentDetails.filter((item, index) => index < 5).map((filteredItem) => {
+          setStudentDetails(studentDetails);
+          console.log(studentDetails);
+        })
+  }
+
+  return (
+    <>
+    <div className="divCount">
+        <div className="count">
+        <label>Count :</label>
+        <input type="text" onChange={counterHandler}></input>
+        </div>
+    </div>
+    <div className="divSection">
+      {studentDetails.map((item: any, key: any) => [
+        <div className="section" key={key}>
+          <div>Name : {item.name}</div>
+          <div>Phone Number :{item.phone}</div>
+          <div>E-Mail : {item.email}</div>
+          <div>Zip Code : {item.address.zipcode}</div>
+        </div>
+      ])}
+    </div>
+    </>
+  );
+};
+
+export default UserDetails;
