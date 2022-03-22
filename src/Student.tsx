fbecb2b3706d7625ad4a3f28d547ac668497fd76
Britto-T <meetbrittohere@gmail.com>
2022-03-22 17:24:19 +0530

    new functions added

--- src/Student.tsx
+++ src/Student.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const Login = () => {
   const [studentDetails, setStudentDetails] = useState<any>([]);
+  const [limiter, setLimiter] = useState(0);
   const {
     register,
     handleSubmit,
@@ -13,42 +14,60 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
+  function fetchStudentData(data:any,limit:any){
+    const studentData: any = [];
+      data.slice(0,limit).map((item: any) => {    
+        studentData.push({"name":item?.name,"phone":item?.phone,"email":item?.email,"zipcode":item?.address?.zipcode})
+      });
+    return studentData;
+  }
+
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then((response: any) => {
-        console.log(response.data);
-        debugger;
-        response.data.map((item: any, key: any) => {
-          return(
-            setStudentDetails([...studentDetails,{"name":item.name,"phone":item.phone,"email":item.email,"zipcode":item.address.zipcode}])
-          );
-        });
-        console.log(studentDetails);
+        const studentData = fetchStudentData(response.data,response.data.length)
+        setStudentDetails(studentData);
+        setLimiter(studentData.length);
       })
       .catch((error: any) => {
-        alert("Invalid User");
+        alert(error);
       });
+      setValue("name","Britto")
   }, []);
 
   const onFormSubmit = (data: any) => {
     setStudentDetails([
-      ...studentDetails,
       {
         name: data.name,
         phone: data.phone,
         email: data.email,
         zipcode: data.zipcode,
       },
+      ...studentDetails
     ]);
   };
+
+  const ChangeLimit=(event:any)=>{
+    //setLimiter(event.target.value);
+    axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response: any) => {
+      const studentData = fetchStudentData(response.data,event.target.value)
+      setStudentDetails(studentData);
+      setLimiter(studentData.length);
+    })
+    .catch((error: any) => {
+      alert(error);
+    });
+  }
 
   return (
     <>
       <form onSubmit={handleSubmit(onFormSubmit)}>
         <div className="fieldWrapper">
           <div>
-            <label>User Name</label>
+            <label>Student Name</label>
           </div>
           <div>
             <input type="text" {...register("name")} placeholder="User Name" />
@@ -81,7 +100,7 @@ const Login = () => {
           </div>
           <div>
             <button className="btnSubmit">Submit</button>
-            <button className="btnReset">Reset</button>
+            <button type="reset" className="btnReset">Reset</button>
           </div>
         </div>
       </form>
@@ -89,7 +108,7 @@ const Login = () => {
       <div className="divCount">
         <div>
           <label>Count</label>
-          <input type="text"></input>
+          <input type="text" onChange={ChangeLimit}></input>
         </div>
       </div>
 
